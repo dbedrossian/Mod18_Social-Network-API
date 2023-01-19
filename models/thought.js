0e23c664b198197ef6ed4a5ea5951e28174cd15d
dbedrossian <danny.bedrossian@gmail.com>
2023-01-18 22:14:47 -0500

    Fixed index.js files

--- models/thought.js
+++ models/thought.js
@@ -1,27 +1,5 @@
 const { Schema, model } = require('mongoose');
 
-const reactionSchema = new Schema(
-  {
-    reactionID: {
-      type: Schema.Types.ObjectId,
-      default: function () { return new ObjectId() },
-    },
-    reactionBody: {
-      type: String,
-      required: true,
-      maxLength: 280,
-    },
-    username: {
-      type: String,
-      required: true,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      // getter method to format the timestamp
-    }
-  }
-)
 const thoughtSchema = new Schema(
   {
     thoughtText: {
